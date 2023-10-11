68f41212920e835cb2a0279d7a023a969404f01d
Diego Monsalve <diales1981@gmail.com>
2023-10-10 20:43:44 -0500

    Finish base

---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,26 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+import { v4 as uuid} from 'uuid'
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css'],
+})
+export class ListComponent {
+  @Output()
+  public onDeleteId: EventEmitter<string> = new EventEmitter();
+
+  @Input()
+  public characterList: Character[] = [
+      {
+      id:uuid(),
+      name: 'Trunks',
+      power: 10,
+    },
+  ];
+
+  onDeleteCharacter(id: string): void {
+    this.onDeleteId.emit(id);
+  }
+}
--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,5 +1,5 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { HeroComponent } from './hero/hero.component';
 import { ListComponent } from './list/list.component';
